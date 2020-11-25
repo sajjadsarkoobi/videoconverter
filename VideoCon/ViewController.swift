46cd7cc395da3151708aec8cd0ec2138e9daa3c9
sajjadsarkoobi <sajjadsarkoobi@gmail.com>
2020-11-25 19:19:20 +0300

    Initial Commit

---
+++ VideoCon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoCon
+//
+//  Created by Sajjad Sarkoobi on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
